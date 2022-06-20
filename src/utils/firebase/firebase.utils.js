e78c3137542bf96ee4cb9cade653a9a7fe469f60
coelho-na <nadinecoelho9@gmail.com>
2022-06-20 20:31:44 -0300

    added sign in component and firebase utils and brought in firebase database

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -7,6 +7,13 @@ import {
   GoogleAuthProvider
 } from 'firebase/auth';
 
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc
+} from 'firebase/firestore'
+
 const firebaseConfig = {
   apiKey: "AIzaSyD_YuTPy-9QuIG1xAdDYm3LP_UTIlEDcn4",
   authDomain: "crown-clothing-db-d4bc6.firebaseapp.com",
@@ -25,3 +32,29 @@ googleProvider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider)
+
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid );
+  const userSnapShot = await getDoc(userDocRef);
+ 
+  if(!userSnapShot.exists()){
+    const {displayName, email} = userAuth;
+    const createdAt = new Date();
+
+    try{
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt
+      })
+    }catch (err){
+      console.log('error creating the user', err.message);
+
+    }
+  }
+
+  return userDocRef;
+}
